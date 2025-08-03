ce4b5d5b2e0d966ac7c887d4cf31c63f45eaec65
yiping0liu <yiping0liu@gmail.com>
2025-08-03 13:55:40 +0000

    Deploying to gh-pages from @ yiping0liu/yiping0liu.github.io@f3dfc558c476c5a957eb3ce186aeef72fddd7546 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,13 +462,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=2eX_3XEAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("http://csee.hnu.edu.cn/people/liuyiping", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
