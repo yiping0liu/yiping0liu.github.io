b7c739d56e8aeb3bf9b659709e891133799596bd
yiping0liu <yiping0liu@gmail.com>
2025-08-03 11:39:07 +0000

    Deploying to gh-pages from @ yiping0liu/yiping0liu.github.io@a805487f987d29abbe620bf8f6f1e5c1c9d67afe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitae",
+          title: "curriculum vitae",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
